05ae257e71a1d205c3f98b0b4da511e2fb3160fa
Sravan Surya <vsravansurya@gmail.com>
2026-01-22 02:21:30 +0530

    Added background wallpaper & Dock

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.scss";
+import React from 'react'
+import Dock from "./components/Dock";
+
+const App = () => {
+  return (
+    <main>
+      <Dock />
+    </main>
+  )
+}
+
+export default App
