e850ae7c937b5fb206c5212ea33ef1e997e70d9a
Sravan Surya <vsravansurya@gmail.com>
2026-02-18 10:21:35 +0530

    Add resizable and draggable MacWindow component using react-rnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import "./app.scss";
 import React from 'react'
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
+import MacWindow from "./components/windows/MacWindow";
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+
+      <MacWindow>Hello</MacWindow>
     </main>
   )
 }
